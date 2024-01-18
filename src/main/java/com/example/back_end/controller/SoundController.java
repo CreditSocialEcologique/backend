b9a3fc6df7489808c0a1c2782c6537e6b6c96630
Steenall <12674641+Steenall@users.noreply.github.com>
2024-01-18 16:29:50 +0100

    Ajout gitignore + soundController

--- src/main/java/com/example/back_end/controller/SoundController.java
+++ src/main/java/com/example/back_end/controller/SoundController.java
@@ -1,28 +1,39 @@
 package com.example.back_end.controller;
 
-import com.example.back_end.model.LitteringIncident;
-import com.example.back_end.model.sound;
+import com.example.back_end.model.BddScores;
+import com.example.back_end.repository.BddScoresRepository;
+import com.example.back_end.service.BddScoresService;
+import com.example.back_end.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class SoundController {
 
-    @PostMapping("/api/sound-level")
-    public String receiveSoundData(@RequestBody sound soundData) {
-        // Process the data
-        processSoundData(soundData);
+    @Autowired
+    private BddScoresService bddScoresService;
+
+    @PostMapping("/api/sound/reduceScore")
+    public String receiveSoundData(@RequestParam(name="id_user") int id_user) {
+
+        Optional<BddScores> user = bddScoresService.getScores(id_user);
+
+        if(user.isEmpty()) {
+            return "Error, user doesn't exist";
+        }else {
+            BddScores bddScores = user.get();
+            bddScores.setNuisanceSonore(bddScores.getNuisanceSonore() - 1);
+            bddScores.calculateFinalScore();
+            bddScoresService.save(bddScores);
+        }
+
+        //Prnd le score sound et on le réduit de 1
 
         return "Data received";
-    }
-
-    private void processSoundData( sound soundData) {
-        // Implement your logic here
-        double volume = soundData.getVolume();
-        // Example: print out the volume
-        System.out.println("Received volume: " + volume);
-
-        // Add your existing logic to process the volume data
     }
 }
