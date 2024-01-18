e420aa2126c8390db9125eb0f33dee1e256f81d9
jlemoine <julien.lemoine@ecole.ensicaen.fr>
2024-01-18 16:53:22 +0100

    more endpoints

--- src/main/java/com/example/back_end/controller/UserController.java
+++ src/main/java/com/example/back_end/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.back_end.controller;
 
+import com.example.back_end.model.BddScores;
 import com.example.back_end.model.User;
 import com.example.back_end.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,31 @@ public class UserController {
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = userService.findAllUsers();
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.findUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        User updatedUser = userService.findUserById(id);
+        updatedUser.setAdresse(newUser.getAdresse());
+        updatedUser.setAge(newUser.getAge());
+        updatedUser.setNom(newUser.getNom());
+        updatedUser.setPrenom(newUser.getPrenom());
+        updatedUser.setCompteBancaire(newUser.getCompteBancaire());
+        updatedUser.setDateNaissance(newUser.getDateNaissance());
+        updatedUser.setMobile(newUser.getMobile());
+        updatedUser.setNuisanceSonore(newUser.getNuisanceSonore());
+        updatedUser.setPollutionLumineuse(newUser.getPollutionLumineuse());
+        updatedUser.setPositionActuelle(newUser.getPositionActuelle());
+        updatedUser.setScoreAlimentation(newUser.getScoreAlimentation());
+        updatedUser.setScoreEnergie(newUser.getScoreEnergie());
+        updatedUser.setScoreTransport(newUser.getScoreTransport());
+        userService.saveUser(updatedUser);
+        return ResponseEntity.ok(updatedUser);
     }
 }
--- src/main/java/com/example/back_end/service/UserService.java
+++ src/main/java/com/example/back_end/service/UserService.java
@@ -16,4 +16,12 @@ public class UserService {
     public List<User> findAllUsers() {
         return userRepository.findAll();
     }
+
+    public User findUserById(long id) {
+        return userRepository.findById(id).get();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }
