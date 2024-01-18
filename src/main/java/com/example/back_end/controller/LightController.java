ffe6050884f2bd5e8ac48470e1537af613163e4f
Steenall <12674641+Steenall@users.noreply.github.com>
2024-01-18 17:43:36 +0100

    Ajout LightController

---
+++ src/main/java/com/example/back_end/controller/LightController.java
@@ -0,0 +1,36 @@
+package com.example.back_end.controller;
+
+import com.example.back_end.model.BddScores;
+import com.example.back_end.service.BddScoresService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class LightController {
+
+    @Autowired
+    private BddScoresService bddScoresService;
+
+    @PostMapping("/api/light/reduceScore")
+    public String receiveLightData(@RequestParam(name="id_user") int id_user) {
+
+        Optional<BddScores> user = bddScoresService.getScores(id_user);
+
+        if(user.isEmpty()) {
+            return "Error, user doesn't exist";
+        } else {
+            BddScores bddScores = user.get();
+            bddScores.setPollutionLumineuse(bddScores.getPollutionLumineuse() - 1);
+            bddScores.calculateFinalScore();
+            bddScoresService.save(bddScores);
+        }
+
+        // Réduit le score de pollution lumineuse de 1
+
+        return "Data received";
+    }
+}
